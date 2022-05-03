dabca024429bd393a8007b4b227a58b8b7f42b9a
Wataru Esaki <build.wataru@gmail.com>
2022-05-03 18:49:44 +0900

    refactor: add constant

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,6 @@
 const mix = require('laravel-mix')
 const MixGlob = require('laravel-mix-glob')
 const mixGlob = new MixGlob({ mix })
+const ASSETS_PATH = 'out/assets'
 
-mixGlob.sass('src/sass/**/*.scss', 'out/assets/css')
+mixGlob.sass('src/sass/**/*.scss', `${ASSETS_PATH}/css`)
