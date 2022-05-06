a39846caf678902841d7b668fcdd769de10c7235
Wataru Esaki <build.wataru@gmail.com>
2022-05-06 10:56:54 +0900

    refactor: laravel mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,17 @@
 'use strict'
 
+const config = require('./config')
 const mix = require('laravel-mix')
 const glob = require('glob')
-const ASSETS_PATH = 'out/assets'
 
 glob
-  .sync('src/sass/**/*.scss', {
+  .sync(config.src.sass, {
     ignore: 'src/sass/**/_*.scss',
   })
   .map((file) =>
-    mix.sass(file, `${ASSETS_PATH}/css`).sourceMaps(false, 'inline-source-map')
+    mix.sass(file, config.out.sass).sourceMaps(false, 'inline-source-map')
   )
 
-glob.sync('src/js/*.js').map((file) => mix.js(file, `${ASSETS_PATH}/js`))
+glob.sync('src/js/*.js').map((file) => mix.js(file, config.out.js))
 
 mix.browserSync(require('./bs-config'))
