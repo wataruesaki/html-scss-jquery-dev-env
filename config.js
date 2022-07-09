4cd2d2f802907152d0eaba65de2473ad380561ed
Wataru Esaki <74096818+wataruesaki@users.noreply.github.com>
2022-07-10 05:43:49 +0900

    feat: configure variables for ejs

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   src: {
-    html: 'src/html/**/*.html',
+    ejs: 'src/ejs/**/*.ejs',
     sass: 'src/sass/**/*.scss',
     js: 'src/js/**/*.js',
     img: 'src/images/**/*.{jpg,jpeg,png,gif,svg}',
   },
   out: {
     root: 'out',
-    html: 'out',
+    ejs: 'out',
     sass: 'out/assets/css',
     js: 'out/assets/js',
     img: 'out/assets/images',
